b18a8e0a1ef509bd9fb5b72bbc6c05439fd43911
Zhihui Tang <crafttang@gmail.com>
2016-12-16 13:51:32 +0100

    opt @IBDesignable and @IBInspectable

--- Apollo/FaceView.swift
+++ Apollo/FaceView.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
+@IBDesignable
 class FaceView: UIView {
+    @IBInspectable
     var scale: CGFloat = 0.90
-
+    @IBInspectable
+    var mouthCurvature: Double = 1.0  // 1 full smile, -1 full frown
+    @IBInspectable
+    var eyesOpen: Bool = true
+    @IBInspectable
+    var eyeBrowTilt: Double = -0.5  //-1, 1
+    @IBInspectable
+    var color: UIColor = UIColor.blue
+    @IBInspectable
+    var linewidth: CGFloat = 5.0
+    
+    
     private var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) * scale / 2
     }
@@ -24,6 +37,8 @@ class FaceView: UIView {
         static let SkullRadiusToMouthWidth: CGFloat = 1
         static let SkullRadiusToMouthHeight: CGFloat = 3
         static let SkullRadiusToMouthOffset: CGFloat = 3
+        static let SkullRadiusToBrowOffset: CGFloat = 5
+        
     }
     
     private enum Eye {
@@ -38,7 +53,7 @@ class FaceView: UIView {
             startAngle: 0.0,
             endAngle: CGFloat(2*M_PI),
             clockwise: false)
-        path.lineWidth = 5.0
+        path.lineWidth = linewidth
         
         return path
     }
@@ -58,7 +73,15 @@ class FaceView: UIView {
     private func pathForEye(eye: Eye) -> UIBezierPath {
         let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
         let eyeCenter = getEyeCenter(eye: eye)
-        return pathForCircleCenteredAtPoint(midPoind: eyeCenter, withRadius: eyeRadius)
+        if eyesOpen {
+            return pathForCircleCenteredAtPoint(midPoind: eyeCenter, withRadius: eyeRadius)
+        } else {
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
+            path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
+            path.lineWidth = linewidth
+            return path
+        }
     }
     
     private func pathForMouth() -> UIBezierPath {
@@ -68,7 +91,6 @@ class FaceView: UIView {
         
         let mouthRect = CGRect(x: skullCenter.x - mouthWidth/2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
         //return UIBezierPath(rect: mouthRect)
-        let mouthCurvature: Double = 1.0  // 1 full smile, -1 full frown
         let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
         let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
         let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
@@ -78,16 +100,44 @@ class FaceView: UIView {
         let path = UIBezierPath()
         path.move(to: start)
         path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
-        path.lineWidth = 5.0
+        path.lineWidth = linewidth
         
         return path
     }
     
+    private func pathForBrow(eye: Eye) -> UIBezierPath {
+        var tilt = eyeBrowTilt
+        switch eye {
+        case .Left: tilt *= -1.0
+        case .Right: break
+        }
+        
+        var browCenter = getEyeCenter(eye: eye)
+        browCenter.y -= skullRadius / Ratios.SkullRadiusToBrowOffset
+        let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
+        let tiltOffset = CGFloat(max(-1, min(tilt,1))) * eyeRadius / 2
+        let browStart = CGPoint(x: browCenter.x - eyeRadius, y: browCenter.y - tiltOffset)
+        let browEnd = CGPoint(x: browCenter.x + eyeRadius, y: browCenter.y + tiltOffset)
+        let path = UIBezierPath()
+        path.move(to: browStart)
+        path.addLine(to: browEnd)
+        path.lineWidth = linewidth
+        return path
+        
+    }
+    
     override func draw(_ rect: CGRect) {
-        UIColor.blue.set()
+        
+        
+        color.set()
         pathForCircleCenteredAtPoint(midPoind: skullCenter, withRadius: skullRadius).stroke()
+        
         pathForEye(eye: .Left).stroke()
         pathForEye(eye: .Right).stroke()
+        
+        pathForBrow(eye: .Left).stroke()
+        pathForBrow(eye: .Right).stroke()
+        
         pathForMouth().stroke()
     }
 
