ec3f8de519aca610d73da6179858443466de17ad
Zhihui Tang <crafttang@gmail.com>
2017-11-20 23:44:04 +0100

    aaa

--- Apollo/RoundImageView.swift
+++ Apollo/RoundImageView.swift
@@ -13,32 +13,44 @@ class RoundImageView: UIView {
     @IBInspectable
     var image: UIImage? {
         didSet {
-            setNeedsLayout()
+            imageView.image = image
         }
     }
     @IBInspectable
     var radius: CGFloat = 50.0 {
         didSet {
-            setNeedsLayout()
+            outerView.layer.cornerRadius = radius
+            outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: radius).cgPath
+            imageView.layer.cornerRadius = radius
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat = 5.0 {
+        didSet {
         }
     }
     
     @IBInspectable
     var bgColor: UIColor = .white {
         didSet {
-            setNeedsLayout()
+            outerView.backgroundColor = bgColor
         }
     }
     
     @IBInspectable
     var borderWidth: CGFloat = 2.0 {
         didSet {
-            setNeedsLayout()
+            outerView.layer.borderWidth = borderWidth
         }
     }
     
     @IBInspectable
-    var borderColor: UIColor = .white { didSet { setNeedsLayout() } }
+    var borderColor: UIColor = .white {
+        didSet {
+            outerView.layer.borderColor = borderColor.cgColor
+        }
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -52,35 +64,28 @@ class RoundImageView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        setupLayer()
+        outerView.frame = self.bounds
+        imageView.frame = self.bounds
     }
     
     private var outerView: UIView = UIView()
     private var imageView: UIImageView = UIImageView()
     
     private func setupLayer() {
-        outerView.frame = self.bounds
+        imageView.clipsToBounds = true
         outerView.clipsToBounds = false
+        
         outerView.layer.shadowColor = UIColor.black.cgColor
         outerView.layer.shadowOpacity = 1
         outerView.layer.shadowOffset = CGSize.zero
-        outerView.layer.shadowRadius = 5
-        outerView.layer.borderColor = borderColor.cgColor
-        outerView.layer.borderWidth = borderWidth
-        outerView.layer.cornerRadius = radius
-        outerView.backgroundColor = bgColor
-        outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: radius).cgPath
-        
-        imageView.frame = self.bounds
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = radius
-        imageView.image = image
+        outerView.layer.shadowRadius = shadowRadius
+
     }
     
     private func commonInit() {
+        setupLayer()
         outerView.addSubview(imageView)
         self.addSubview(outerView)
         
-        setupLayer()
     }
 }
